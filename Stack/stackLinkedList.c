b52693029db11e0539c840e10012420804abe02a
AryanChoudhary2807 <155878910+AryanChoudhary2807@users.noreply.github.com>
2024-01-07 15:31:50 +0530

    Create stackLinkedList.c

---
+++ Stack/stackLinkedList.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *top=NULL;
+
+
+void push(int data){
+    struct node *temp = (struct node*) malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next = top;
+    top = temp;
+    
+}
+
+void pop(){
+    top=top->next;
+}
+
+int  peek(){
+    return top->data;
+}
+
+int main() {
+    // Write C code here
+    printf("Stack\n");
+    
+    push(3);
+    push(2);
+    push(1);
+    
+    while(top!=NULL){
+        printf(" %d \n",peek());
+        pop();
+    }
+
+
+    return 0;
+}
